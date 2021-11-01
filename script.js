59fd36ffa8bc3fe2c5b38ff3c537235fb35431cd
Roshin Roychan <79758267+TheRoshin@users.noreply.github.com>
2021-11-01 16:41:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ function lastedit(){
   var lastestDate = new Date(document.lastModified);
   document.getElementById("time").innerHTML = "Last modified on: " + lastestDate;
 }
+
+/* function for change the color with button*/
+function redColor(){
+  document.getElementById("button").style.backgroundColor = "red";
+}
+
+function blueColor(){
+  document.getElementById("button").style.backgroundColor = "blue";
+}
+
+function yellowColor(){
+  document.getElementById("button").style.backgroundColor = "yellow";
+}
+
+
 /*test case*/
 function text(){
   console.log("it is working");
 }
 
+
 /*calling the function*/
 lastedit();
+/*redColor();
+greenColor();
+yellowColor();*/
 text();
